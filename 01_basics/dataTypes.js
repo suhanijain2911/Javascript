638520b91718897865016e73447ef1ab63581e5d
suhanijain2911 <suhanijain2911@gmail.com>
2026-09-25 20:02:20 +0000

    DataTypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,29 @@
+"use script"; // treat all JS code as a newer version through engine  
+
+// alert(3+3) we're using node js not browser (browser me inspect par jaakr agar
+//  hum ye statement likhenge toh output me 6 aayega like ek msg pop up hoga 6 ka )
+
+//code must be readable or futureproof. that's why we don't use this type of statements
+console.log(3
+    +3) // code readibility should be high
+
+console.log("Suhani") 
+
+let name = "Suhaniii" 
+let age = 20 
+let isLoggedIn = false // jbh user website par log in hoga ya nahi tbh iska use krenge 
+
+// number => range: 2 to the power 52 (range joh memory address me milta hai)
+// bighInt => when the number is too large used in trading, website like facebook or reddit etc. 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned yet
+// symbol => uniqueness
+
+console.log(typeof "sj");
+console.log(typeof null); // Null is an object 
+console.log(typeof undefined); // But Undefined is a type in itself.
+
+
+
